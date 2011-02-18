d2bf677b32920dff4eae49b607069428608e5c1e
nmonje <nicholas.monje@gmail.com>
2011-02-18 13:25:48 -0500

    Ran rails new here.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jigimojo::Application.config.secret_token = '96145ab67215b9db4d309d904ad05dcaa78106dd3806ba111f6074daac6254af1ad2d403ea79224b71742175b0bc57ff53e44d90d39d0b897e0dcb6e583d1d48'
